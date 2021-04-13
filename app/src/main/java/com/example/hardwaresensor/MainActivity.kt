d0290b0a4f7c60123b2607599ee6f16cc94e61ac
Mehul-Bisht <mehulbishtanna@gmail.com>
2021-04-14 01:24:51 +0530

    check for sensor

--- app/src/main/java/com/example/hardwaresensor/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensor/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.hardwaresensor
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        val sensorManager = getSystemService<SensorManager>()
+        if(sensorManager == null) {
+            Toast.makeText(this,"Couldn't get Sensors", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            val sensors = sensorManager.getSensorList(Sensor.TYPE_ALL)
+            sensors.forEach {
+                Log.d("Sensor ", """
+                    ${it.name} | ${it.stringType} | ${it.vendor}
+                """.trimIndent())
+            }
+        }
     }
 }
