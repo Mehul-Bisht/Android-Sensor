0bde34452f5ea473b8d242271bcfaa56974081d3
Mehul-Bisht <mehulbishtanna@gmail.com>
2021-04-14 01:46:28 +0530

    added senor event listener

--- app/src/main/java/com/example/hardwaresensor/MainActivity.kt
+++ app/src/main/java/com/example/hardwaresensor/MainActivity.kt
@@ -1,14 +1,49 @@
 package com.example.hardwaresensor
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sensorEventListener: SensorEventListener
+    lateinit var sensorManager: SensorManager
+    lateinit var proximitySensor: Sensor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        sensorManager = getSystemService<SensorManager>()!!
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)!!
+
+        sensorEventListener = object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                //
+            }
+
+            override fun onSensorChanged(event: SensorEvent?) {
+                Log.d("Sensor ","""
+                    ${event!!.values[0]}
+                """.trimIndent())
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(
+                sensorEventListener,proximitySensor,1000*1000
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(sensorEventListener)
     }
 }
